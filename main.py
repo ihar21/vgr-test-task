cca850aa3813d7ef065852b21a0b30ce70cc3e06
ihar21 <gogakharitonov@gmail.com>
2025-10-18 18:12:06 +0300

    Аунтефікація Ґуґл

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("test")
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request 
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def authenticate():
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return build('drive', 'v3', credentials=creds)
+
+if __name__ == "__main__":
+    service = authenticate()
+    print("This is the main module.")
