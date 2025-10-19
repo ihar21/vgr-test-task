f22c718dcc43d20000bb4da2ade613c0fee5ace5
ihar21 <gogakharitonov@gmail.com>
2025-10-19 19:29:26 +0300

    DownloadIo/UploadtxtIo/BaseProccesing

---
+++ config.py
@@ -0,0 +1,12 @@
+#AUTHENTICATION
+TOKEN_FILE = "token.json"
+CREDENTIALS_FILE = "credentials.json"
+SCOPES = ['https://www.googleapis.com/auth/drive']
+#FOLDERS ID
+SOURCE_ROOT_FOLDER_ID = "1R3hDscEc_Ujh1FytqWROg4tS__qcO1Ub"
+SOURCE_AUDIO_FOLDER_ID = "1dpKG-eaFg2glOovkI4sYgLyPo3mW9Ilg"
+DESTINATION_ROOT_FOLDER_ID = "1PsjppOUaAbFqf4sjcZE8t1jyNaqJYfXV"
+#GEmini API
+GENAI_API_KEY = "YOUR_GENAI_API_KEY"
+GENAI_MODEL = "gemini-2.5-flash"
+MIME_TYPE = "audio/mp3"
--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import os
+import io
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request 
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseUpload, MediaIoBaseDownload
+from google import genai
+from google.genai import types
+from config import TOKEN_FILE, CREDENTIALS_FILE, SCOPES, SOURCE_AUDIO_FOLDER_ID, DESTINATION_ROOT_FOLDER_ID, GENAI_API_KEY, GENAI_MODEL, MIME_TYPE
+from promts import PROMT
 
-#--SETTINGS--
-#>AUTHENTICATION
-TOKEN_FILE = "token.json"
-CREDENTIALS_FILE = "credentials.json"
-SCOPES = ['https://www.googleapis.com/auth/drive']
-#>FOLDERS ID
-SOURCE_ROOT_FOLDER_ID = "1R3hDscEc_Ujh1FytqWROg4tS__qcO1Ub"
-SOURCE_AUDIO_FOLDER_ID = "1dpKG-eaFg2glOovkI4sYgLyPo3mW9Ilg"
-DESTINATION_AUDIO_FOLDER_ID = ""
-DESTINATION_ROOT_FOLDER_ID = "1PsjppOUaAbFqf4sjcZE8t1jyNaqJYfXV"
-#--SETTINGS END--
+destination_audio_folder = None
 
 #--FUNCTIONS--
 def authenticate():
@@ -52,6 +47,29 @@ def upload_file(service, file_path, parent_id):
     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     return file.get('id')
 
+def upload_bytes(service, file_name, file_bytes, parent_id):
+    file_metadata = {
+        'name': file_name,
+        'parents': [parent_id]
+    }
+    media = MediaIoBaseUpload(io.BytesIO(file_bytes), mimetype='application/octet-stream', resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    return file.get('id')
+
+def download_audio_bytes(service, file_id):
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+    fh.seek(0)
+    return fh.read()
+
+def get_file_name(service, file_id):
+    file = service.files().get(fileId=file_id, fields='name').execute()
+    return file.get('name')
+
 def list_files_in_folder(service, folder_id):
     query = f"'{folder_id}' in parents and trashed=false"
     results = service.files().list(q=query, fields="files(id, name)").execute()
@@ -65,10 +83,39 @@ def copy_file(service, file_id, new_name, parent_id):
     copied_file = service.files().copy(fileId=file_id, body=file_metadata).execute()
     return copied_file.get('id')
 
-if __name__ == "__main__":
+def LLM_transcribe_and_analyze(client, audio_bytes, prompt):
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=[
+                prompt,
+                types.Part.from_bytes(
+                    data=audio_bytes,
+                    mime_type=MIME_TYPE,
+                ),
+            ],
+        )
+        return response.text
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+def main():
     service = authenticate()
-    DESTINATION_AUDIO_FOLDER_ID = create_folder(service, "Records", DESTINATION_ROOT_FOLDER_ID)
+    #destination_audio_folder = create_folder(service, "Records", DESTINATION_ROOT_FOLDER_ID)
     result=list_files_in_folder(service, SOURCE_AUDIO_FOLDER_ID)
-    for file in result:
+    #test_text = b"Test upload from bytes"
+    #upload_bytes(service, "test_bytes_upload.txt", test_text, DESTINATION_ROOT_FOLDER_ID)
+    #for file in result:
         #print(f"Found file: {file['name']} (ID: {file['id']})")
-        copy_file(service, file['id'], file['name'], DESTINATION_AUDIO_FOLDER_ID)
+        #copy_file(service, file['id'], file['name'], DESTINATION_AUDIO_FOLDER_ID)
+    #copy_file(service, '12RRthiUDo98R2ZCs13YL2ktdzfQnhCT9', '2025-09-10_15-52_0632838007_incoming.mp3', destination_audio_folder)
+    result=list_files_in_folder(service, "1St5Zse4pBw34MYjG6wT7-pYlH2a3P2Jw")
+    #print(result[0]['id'])
+    client = genai.Client(api_key=GENAI_API_KEY)
+    audio_bytes = download_audio_bytes(service, result[0]['id'])
+    transcription = LLM_transcribe_and_analyze(client, audio_bytes, PROMT)
+    upload_bytes(service, "transcription.txt", transcription.encode('utf-8'), "1St5Zse4pBw34MYjG6wT7-pYlH2a3P2Jw")
+
+
+if __name__ == "__main__":
+    main()
